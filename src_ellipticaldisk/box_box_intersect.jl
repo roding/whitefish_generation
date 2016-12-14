6d613519339d24491a1cc40d76f874b8708c5815
Magnus Röding <magnus.roding@gmail.com>
2016-12-14 12:13:32 +0100

    Modified box-box intersection test function

--- src_ellipticaldisk/box_box_intersect.jl
+++ src_ellipticaldisk/box_box_intersect.jl
@@ -21,7 +21,7 @@ function box_box_intersect(	lbx1::Float64,
 							lbz2::Float64,
 							ubz2::Float64)
 
-  return 	(2.0 * abs( (lbx1 + ubx1) / 2.0 - (lbx2 + ubx2) / 2.0 ) <= ubx1 - lbx1 + ubx2 - lbx2 ) &&
-			(2.0 * abs( (lby1 + uby1) / 2.0 - (lby2 + uby2) / 2.0 ) <= uby1 - lby1 + uby2 - lby2 ) &&
-			(2.0 * abs( (lbz1 + ubz1) / 2.0 - (lbz2 + ubz2) / 2.0 ) <= ubz1 - lbz1 + ubz2 - lbz2 )
+  return 	(abs( (lbx1 + ubx1) - (lbx2 + ubx2)) <= ubx1 - lbx1 + ubx2 - lbx2 ) &&
+			(abs( (lby1 + uby1) - (lby2 + uby2)) <= uby1 - lby1 + uby2 - lby2 ) &&
+			(abs( (lbz1 + ubz1) - (lbz2 + ubz2)) <= ubz1 - lbz1 + ubz2 - lbz2 )
 end
--- src_ellipticaldisk/test_box_box_intersect.jl
+++ src_ellipticaldisk/test_box_box_intersect.jl
@@ -7,27 +7,27 @@ function test_box_box_intersect()
 	lby1::Float64 = 0.0
 	uby1::Float64 = 1.0
 	lbz1::Float64 = 0.0
-	ubz1::Float64 = 2.0
+	ubz1::Float64 = 1.0
 	
-	lbx2::Float64 = 0.0
-	ubx2::Float64 = 1.0
-	lby2::Float64 = 0.0
-	uby2::Float64 = 1.0
-	lbz2::Float64 = 0.0
-	ubz2::Float64 = 2.0
+	lbx2::Float64 = -1.0
+	ubx2::Float64 = -0.01
+	lby2::Float64 = -1.0
+	uby2::Float64 = 0.0
+	lbz2::Float64 = -1.0
+	ubz2::Float64 = 0.0
 	
-	is_intersecting = boxbox_intersect(	lbx1,
-									ubx1,
-									lby1,
-									uby1,
-									lbz1,
-									ubz1,
-									lbx2,
-									ubx2,
-									lby2,
-									uby2,
-									lbz2,
-									ubz2)
+	is_intersecting = box_box_intersect(	lbx1,
+										ubx1,
+										lby1,
+										uby1,
+										lbz1,
+										ubz1,
+										lbx2,
+										ubx2,
+										lby2,
+										uby2,
+										lbz2,
+										ubz2)
 										
 	println(is_intersecting)
 
