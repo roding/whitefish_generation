daa4bb56d2e9704ef6c5f2d0c2725a24ec3648e6
Magnus Röding <magnus.roding@gmail.com>
2016-12-14 09:50:21 +0100

    Modified plane-box intersection test function

--- src_ellipticaldisk/test_plane_box_intersect.jl
+++ src_ellipticaldisk/test_plane_box_intersect.jl
@@ -1,33 +1,37 @@
+include("plane_box_intersect.jl")
+
 function test_plane_box_intersect()
 	
 	px::Float64 = 0.0
 	py::Float64 = 0.0
-	pz::Float64 = 0.0
+	pz::Float64 = -0.01
+	
 	nx::Float64 = 1.0
-	ny::Float64 = 0.0
-	nz::Float64 = 0.0
+	ny::Float64 = 1.0
+	nz::Float64 = 1.0
+	
 	lbx::Float64 = 0.0
 	ubx::Float64 = 1.0
 	lby::Float64 = 0.0
 	uby::Float64 = 1.0
 	lbz::Float64 = 0.0
-	ubz::Float64 = 1.0
+	ubz::Float64 = 2.0
 	
-	is_intersecting = plane_box_intersect(	px
-										py
-										pz
-										nx
-										ny
-										nz
-										lbx
-										ubx 
-										lby
-										uby 
-										lbz
+	is_intersecting = plane_box_intersect(	px,
+										py,
+										pz,
+										nx,
+										ny,
+										nz,
+										lbx,
+										ubx,
+										lby,
+										uby,
+										lbz,
 										ubz)
 										
 	println(is_intersecting)
 
 end
 
-is_intersecting()
+test_plane_box_intersect()
