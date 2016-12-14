fcd40b77d5162a38322b871a57472ff3c6e868b7
Magnus Röding <magnus.roding@gmail.com>
2016-12-14 10:14:48 +0100

    Modified box-box intersection function

--- src_ellipticaldisk/box_box_intersect.jl
+++ src_ellipticaldisk/box_box_intersect.jl
@@ -1,4 +1,27 @@
-function box_box_intersect()
+###############################################################################
+#
+# BOX_BOX_INTERSECT
+#
+# Returns whether two boxes, specified by their lower and upper bounds
+# (lbx1, ubx1, lby1, uby1, lbz1, ubz1) and (lbx2, ubx2, lby2, uby2, lbz2, ubz2)
+# are intersecting.
+#
+###############################################################################
 
-	return true
+function box_box_intersect(	lbx1::Float64,
+							ubx1::Float64, 
+							lby1::Float64,
+							uby1::Float64, 
+							lbz1::Float64,
+							ubz1::Float64,
+							lbx2::Float64,
+							ubx2::Float64, 
+							lby2::Float64,
+							uby2::Float64, 
+							lbz2::Float64,
+							ubz2::Float64)
+
+  return 	(2.0 * abs( (lbx1 + ubx1) / 2.0 - (lbx2 + ubx2) / 2.0 ) <= ubx1 - lbx1 + ubx2 - lbx2 ) &&
+			(2.0 * abs( (lby1 + uby1) / 2.0 - (lby2 + uby2) / 2.0 ) <= uby1 - lby1 + uby2 - lby2 ) &&
+			(2.0 * abs( (lbz1 + ubz1) / 2.0 - (lbz2 + ubz2) / 2.0 ) <= ubz1 - lbz1 + ubz2 - lbz2 )
 end
