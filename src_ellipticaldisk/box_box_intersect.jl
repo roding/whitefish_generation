2f9b493478d325918e369817a7331d07b59c6adb
Magnus Röding <magnus.roding@gmail.com>
2016-12-14 12:42:07 +0100

    Modified box-box intersection function to incorporate 'modulo' arithmetic

--- src_ellipticaldisk/box_box_intersect.jl
+++ src_ellipticaldisk/box_box_intersect.jl
@@ -4,7 +4,8 @@
 #
 # Returns whether two boxes, specified by their lower and upper bounds
 # (lbx1, ubx1, lby1, uby1, lbz1, ubz1) and (lbx2, ubx2, lby2, uby2, lbz2, ubz2)
-# are intersecting.
+# are intersecting. The calculation is performed "modulo the simulation domain"
+# i.e. using the closest box image. 
 #
 ###############################################################################
 
@@ -19,9 +20,36 @@ function box_box_intersect(	lbx1::Float64,
 							lby2::Float64,
 							uby2::Float64, 
 							lbz2::Float64,
-							ubz2::Float64)
+							ubz2::Float64,
+							Lx::Float64,
+							Ly::Float64,
+							Lz::Float64)
+							
+	if 0.5 * (lbx1 + ubx1) - 0.5 * (lbx2 + ubx2) < -0.5*Lx
+		lbx2 = lbx2 + Lx
+		ubx2 = ubx2 + Lx
+	elseif 0.5 * (lbx1 + ubx1) - 0.5 * (lbx2 + ubx2) > 0.5*Lx
+		lbx2 = lbx2 - Lx
+		ubx2 = ubx2 - Lx
+	end
+	
+	if 0.5 * (lby1 + uby1) - 0.5 * (lby2 + uby2) < -0.5*Ly
+		lby2 = lby2 + Ly
+		uby2 = uby2 + Ly
+	elseif 0.5 * (lby1 + uby1) - 0.5 * (lby2 + uby2) > 0.5*Ly
+		lby2 = lby2 - Ly
+		uby2 = uby2 - Ly
+	end
+	
+	if 0.5 * (lbz1 + ubz1) - 0.5 * (lbz2 + ubz2) < -0.5*Lz
+		lbz2 = lbz2 + Lz
+		ubz2 = ubz2 + Lz
+	elseif 0.5 * (lbz1 + ubz1) - 0.5 * (lbz2 + ubz2) > 0.5*Lz
+		lbz2 = lbz2 - Lz
+		ubz2 = ubz2 - Lz
+	end
 
-  return 	(abs( (lbx1 + ubx1) - (lbx2 + ubx2)) <= ubx1 - lbx1 + ubx2 - lbx2 ) &&
+	return 	(abs( (lbx1 + ubx1) - (lbx2 + ubx2)) <= ubx1 - lbx1 + ubx2 - lbx2 ) &&
 			(abs( (lby1 + uby1) - (lby2 + uby2)) <= uby1 - lby1 + uby2 - lby2 ) &&
 			(abs( (lbz1 + ubz1) - (lbz2 + ubz2)) <= ubz1 - lbz1 + ubz2 - lbz2 )
 end
