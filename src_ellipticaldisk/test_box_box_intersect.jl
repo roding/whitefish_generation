eacbec51d8e428a8b637f5273d39c51228775371
Magnus Röding <magnus.roding@gmail.com>
2016-12-14 12:07:19 +0100

    Added box-box intersection test function

---
+++ src_ellipticaldisk/test_box_box_intersect.jl
@@ -0,0 +1,37 @@
+include("box_box_intersect.jl")
+
+function test_box_box_intersect()
+	
+	px::Float64 = 0.0
+	py::Float64 = 0.0
+	pz::Float64 = -0.01
+	
+	nx::Float64 = 1.0
+	ny::Float64 = 1.0
+	nz::Float64 = 1.0
+	
+	lbx::Float64 = 0.0
+	ubx::Float64 = 1.0
+	lby::Float64 = 0.0
+	uby::Float64 = 1.0
+	lbz::Float64 = 0.0
+	ubz::Float64 = 2.0
+	
+	is_intersecting = boxbox_intersect(	px,
+										py,
+										pz,
+										nx,
+										ny,
+										nz,
+										lbx,
+										ubx,
+										lby,
+										uby,
+										lbz,
+										ubz)
+										
+	println(is_intersecting)
+
+end
+
+test_box_box_intersect()
