79db2f7c15482ae63b3806dc650c19b1f0e60b48
Magnus Röding <magnus.roding@gmail.com>
2016-12-15 13:17:31 +0100

    Modified version writer

---
+++ src/set_version.jl
@@ -0,0 +1,12 @@
+function set_version()
+	version_major::Int64 = 0
+	version_minor::Int64 = 0
+	version_patch::Int64 = 1
+
+	file_name_version = "version"
+	file_stream_version = open(file_name_version, "w")
+	write(file_stream_version, version_major)
+	write(file_stream_version, version_minor)
+	write(file_stream_version, version_patch)
+	close(file_stream_version)
+end
