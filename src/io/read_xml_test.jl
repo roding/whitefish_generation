de8774cb2903b240bbcdcaabde18c0eb395a04a0
Magnus Röding <magnus.roding@gmail.com>
2016-12-15 16:12:01 +0100

    Introduced ETA in simulation

---
+++ src/io/read_xml_test.jl
@@ -0,0 +1,15 @@
+function read_xml_test()
+	file_name_diffusion = "particle_system.xml"
+	file_stream_diffusion = open(file_name_diffusion, "r")
+	
+	
+	
+	close(file_stream_diffusion)
+
+
+
+
+
+
+
+end
