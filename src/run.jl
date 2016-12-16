f74d42285b2937d0fffb5cc1bcd95d7c1b8b1baf
Magnus Röding <magnus.roding@gmail.com>
2016-12-16 11:17:46 +0100

    ARGS interpretation

--- src/run.jl
+++ src/run.jl
@@ -7,30 +7,40 @@ include("io/read_xml_system.jl")
 #include("ellipticaldisk/*.jl")
 
 function run()
+	# Process input arguments.
 	silent_mode::Bool = false
 	input_file_path::String = ""
-	input_file_ok::Bool = false
+	#input_file_ok::Bool = false
 	
 	number_of_arguments::Int64 = length(ARGS)
 	
 	for current_argument = 1:number_of_arguments
-		if ARGS[current_argument] == "silent"
+		if ARGS[current_argument] == "-silent"
 			silent_mode = true
-		elseif isfile(ARGS[current_argument])
-			input_file_path = ARGS[current_argument]
-			input_file_ok = true
+		elseif ARGS[current_argument][1:7] ="-input="
+			input_file_path = ARGS[current_argument][8:end]
 		end
 	end
+	#input_file_ok = true
+			
 	
-	if !input_file_ok
-		error("No input file specified.")
-	else
-		println("Input file specified.")
+	if !isfile(ARGS[current_argument])
+		println("No input file specified or specified input does not exist. Aborting.")
 	end
 	
+	# Print text header.
 	if !silent_mode
 		print_header()
 	end
+	
+	# Process input file.
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
