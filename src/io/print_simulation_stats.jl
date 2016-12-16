a39d127a471e51eb2a1f2b02d26cb47e64b6df30
Magnus Röding <magnus.roding@gmail.com>
2016-12-16 13:54:51 +0100

    Constructing main run file

--- src/io/print_simulation_stats.jl
+++ src/io/print_simulation_stats.jl
@@ -1,6 +1,10 @@
-function print_simulation_stats()
+function print_simulation_stats(number_of_particles::Int64, number_of_diffusers::Int64, Lx::Float64, Ly::Float64, Lz::Float64)
+	println("Simulation stats:")
+	println(join(("   Simulation domain size:   x: ", string(Lx))))
+	println(join(("                             y: ", string(Ly))))
+	println(join(("                             z: ", string(Lz))))
+	println(join(("   Number of particles:         ", string(number_of_particles))))
+	println(join(("   Number of diffusers:         ", string(number_of_diffusers))))
 	
-	
-	
-	
+	nothing
 end
--- src/run.jl
+++ src/run.jl
@@ -1,10 +1,10 @@
-#workspace()
-
 include("io/get_version.jl")
 include("io/print_header.jl")
 include("io/read_xml_system.jl")
 include("io/read_xml_input.jl")
 include("io/print_simulation_stats.jl")
+
+include("ellipticaldisk/simulate_diffusion.jl")
 
 function run()
 	# Process input arguments.
@@ -24,32 +24,37 @@ function run()
 		end
 	end
 	
-	# Error checking for input file specification.
-	if !input_file_specified
-		println("No input file specified or specified input does not exist. Aborting.")
-	end
-	
 	# Print text header.
 	if !silent_mode
 		print_header()
 	end
 	
+	# Error checking for input file specification.
+	if !input_file_specified
+		println("No input file specified or specified input does not exist. Aborting.")
+	end
+
 	# Process input file.
+	if !silent_mode
+		println(join(("Reading input from file ", input_file_path, "...")))
+	end
 	(particle_system_path::String, output_path::String, D0::Float64, deltat_coarse::Float64, number_of_time_points_coarse::Int64, number_of_time_points_fine_per_coarse::Int64, number_of_diffusers::Int64, number_of_cells_x::Int64, number_of_cells_y::Int64, number_of_cells_z::Int64) = read_xml_input(input_file_path)
 	
+	# Process particle system file.
+	if !silent_mode
+		println(join(("Reading particle system configuration from file ", particle_system_path, "...")))
+	end
+	(X::Array{Float64, 1}, Y::Array{Float64, 1}, Z::Array{Float64, 1}, THETA1::Array{Float64, 1}, THETA2::Array{Float64, 1}, THETA3::Array{Float64, 1}, R1::Array{Float64, 1}, R2::Array{Float64, 1}, Lx::Float64, Ly::Float64, Lz::Float64) = read_xml_system(particle_system_path)
+	
+	number_of_particles::Int64	 = length(X)
+		
 	# Print simulation stats.
 	if !silent_mode
-		print_simulation_stats()
+		print_simulation_stats(number_of_particles, number_of_diffusers, Lx, Ly, Lz)
 	end
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	# Simulate diffusion.
+	(msd_x::Array{Float64, 1}, msd_y::Array{Float64, 1}, msd_z::Array{Float64, 1}, D0_empirical::Float64) = simulate_diffusion(X, Y, Z, THETA1, THETA2, THETA3, R1, R2, Lx, Ly, Lz, D0, deltat_coarse, number_of_time_points_coarse, number_of_time_points_fine_per_coarse, number_of_diffusers, number_of_cells_x, number_of_cells_y, number_of_cells_z, silent_mode)	
 	
 	# Print output information.
 	if !silent_mode
