4e91ddd34d1f49d88d6f7a52e0a2d05cc479a397
Magnus Röding <magnus.roding@gmail.com>
2017-01-05 13:10:02 +0100

    xml key writer

--- src/io/write_xml_output_generation.jl
+++ src/io/write_xml_output_generation.jl
@@ -5,7 +5,7 @@ function write_xml_output_generation(output_file_path::String)
 
 	
 	
-	@printf(file_stream, "%s", "<output_generation>\n")
+	@printf(file_stream, "%s", "</output_generation>\n")
 	
 	close(file_stream)
 
