cfc4aa14f375f9c53d00c62e1dcf4dbad0feb2cb
Magnus Röding <magnus.roding@gmail.com>
2017-01-05 13:23:20 +0100

    Generalized xml key writer to different data types

--- src/io/write_xml_key.jl
+++ src/io/write_xml_key.jl
@@ -2,7 +2,23 @@ function write_xml_key(file_stream::IOStream, key_name::String, key_value)
 	
 	@printf(file_stream, "%s", "\u0009")
 	@printf(file_stream, "%s", join(("<", key_name, ">")))
-	@printf(file_stream, "%0.3f", key_value)
+	
+	if typeof(key_value) == Float64
+		@printf(file_stream, "%0.6f", key_value)
+	elseif typeof(key_value) == Array{Float64, 1}
+		for i = 1:length(key_value)-1
+			@printf(file_stream, "%0.6f", key_value[i])
+			@printf(file_stream, "%s", ",")
+		end
+		@printf(file_stream, "%0.6f", key_value[end])
+	elseif typeof(key_value) == Int64
+		@printf(file_stream, "%d", key_value)
+	elseif typeof(key_value) == String
+		@printf(file_stream, "%s", key_value)
+	else
+		error("Incompatible data type.")
+	end
+	
 	@printf(file_stream, "%s", join(("</", key_name, ">")))
 	@printf(file_stream, "%s", "\n")
 	
--- src/io/write_xml_output_generation.jl
+++ src/io/write_xml_output_generation.jl
@@ -5,7 +5,12 @@ function write_xml_output_generation(output_file_path::String)
 	
 	@printf(file_stream, "%s", "<output_generation>\n")
 
-	write_xml_key(file_stream, "key", 3.4)
+	write_xml_key(file_stream, "domain_size_x", Lx)
+	write_xml_key(file_stream, "domain_size_y", Ly)
+	write_xml_key(file_stream, "domain_size_z", Lz)
+	write_xml_key(file_stream, "particle_type", "ellipticaldisk")
+	write_xml_key(file_stream, "number_of_particles", number_of_particles)
+	write_xml_key(file_stream, "X", X)
 	
 	@printf(file_stream, "%s", "</output_generation>")
 	
@@ -15,3 +20,19 @@ function write_xml_output_generation(output_file_path::String)
 end
 
 #write_xml_output_generation("test_output.xml")
+
+<particle_system>
+	<domain_size_x>100.0</domain_size_x>
+	<domain_size_y>100.0</domain_size_y>
+	<domain_size_z>100.0</domain_size_z>
+	<type>ellipticaldisk</type>
+	<number_of_particles>1</number_of_particles>
+	<X>10.0</X>
+	<Y>10.0</Y>
+	<Z>10.0</Z>
+	<THETA1>0.0</THETA1>
+	<THETA2>1.0</THETA2>
+	<THETA3>2.0</THETA3>
+	<R1>10.0</R1>
+	<R2>10.0</R2>
+</particle_system>
