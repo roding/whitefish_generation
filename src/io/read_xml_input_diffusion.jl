a49f6dc7657c5bcde0967bc63ba79a6a5ad872f0
Magnus Röding <magnus.roding@gmail.com>
2017-01-05 16:09:43 +0100

    New diffusion input format

--- src/io/read_xml_input_diffusion.jl
+++ src/io/read_xml_input_diffusion.jl
@@ -1,4 +1,4 @@
-function read_xml_input(input_file_path::String)
+function read_xml_input_diffusion(input_file_path::String)
 	file_stream::IOStream = open(input_file_path, "r")
 	file_string::String = readstring(file_stream)
 	close(file_stream)
