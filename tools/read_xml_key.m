037a1be0542bd252ba8c4de089ad1a33b2c2743f
Magnus Röding <magnus.roding@gmail.com>
2017-01-17 18:41:43 +0100

    Successful xml reader of diffusion output

--- tools/read_xml_key.m
+++ tools/read_xml_key.m
@@ -2,22 +2,22 @@ function val = read_xml_key(file_string, key_name, fmt)
 
 pattern_before = ['<' key_name '>'];
 pattern_after = ['</' key_name '>'];
-ind_start = strfind(str, pattern_before) + numel(pattern_before);
-ind_end = strfind(str, pattern_after) - 1;
+ind_start = strfind(file_string, pattern_before) + numel(pattern_before);
+ind_end = strfind(file_string, pattern_after) - 1;
 
 ind = ind_start:ind_end;
 key_string = file_string(ind);
 
 val = nan;
 
-if fmt == 'string'
+if isequal(fmt, 'string')
     val = key_string;
-elseif fmt == 'scalar'
+elseif isequal(fmt, 'scalar')
     val = str2double(key_string);
-elseif fmt == 'array'
-    ind_separator = strfind(substr, ',');
+elseif isequal(fmt, 'array')
+    ind_separator = strfind(key_string, ',');
     val = zeros(numel(ind_separator) + 1, 1);
-    ind_separator = [0 ind_separator numel(substr)+1];
+    ind_separator = [0 ind_separator numel(key_string)+1];
     for i = 1:numel(ind_separator)-1
         val(i) = str2double(key_string(ind_separator(i)+1:ind_separator(i+1)-1));
     end
