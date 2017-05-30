c8686001b42c2b6d92942f8582fd3915edc682ce
Magnus Röding <magnus.roding@gmail.com>
2017-05-30 18:19:45 +0200

    Fixed other problem with git describe

--- src/text_io/get_version.jl
+++ src/text_io/get_version.jl
@@ -1,8 +1,10 @@
 function get_version()
 	cmd::Cmd = `git describe --always`
-	
 	version_string::String = readstring(cmd)
-	version_string = version_string[1:end-2]
+	
+	if version_string[end] == '\n'
+		version_string = version_string[1:end-1]
+	end
 	
 	return version_string
 end
