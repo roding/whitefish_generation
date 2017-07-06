213dff27ddd2ee38af7b531d31688b505d641847
Magnus Röding <magnus.roding@gmail.com>
2017-07-06 11:20:04 +0200

    Removed old code to start over with quaternion-based more modular code

--- src/text_io/print_header.jl
+++
@@ -1,9 +0,0 @@
-include("get_version.jl")
-
-function print_header()
-	version_string::String = get_version()
-	println(join(("This is Whitefish generation module version ", version_string, ".")))
-	println(join(("Executed on Julia version ", string(Sys.VERSION), " (", Sys.MACHINE, ").")))
-	
-	nothing	
-end
