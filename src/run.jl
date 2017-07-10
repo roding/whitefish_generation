585d0f143ee5255461b094481c3dd08e6eb14595
Magnus Röding <magnus.roding@gmail.com>
2017-07-10 10:37:52 +0200

    Changes

--- src/run.jl
+++ src/run.jl
@@ -12,15 +12,19 @@ include("signed_distance_mod.jl")
 include("quaternion_mult.jl")
 
 function run()
+	# Inititalization of random number generation device.
+	random_seed::Int64 = convert(Int64, time_ns())
+	srand(random_seed)
+	
 	# Simulation domain dimensions.
-	Lx::Float64 = 4.0
-	Ly::Float64 = 4.0
-	Lz::Float64 = 4.0
+	Lx::Float64 = 8.0
+	Ly::Float64 = 8.0
+	Lz::Float64 = 8.0
 	
 	# Type of particles.
 	#particle_type::String = "sphere"
-	particle_type::String = "ellipse"
-	#particle_type::String = "ellipsoid"
+	#particle_type::String = "ellipse"
+	particle_type::String = "ellipsoid"
 	
 	particle_type_index::Int64 = 0
 	if particle_type == "sphere"
@@ -32,7 +36,7 @@ function run()
 	end
 	
 	# Number of particles.
-	number_of_particles::Int64 = 64
+	number_of_particles::Int64 = 27
 
 	# Particle property matrix (i.e. radii).
 	number_of_properties::Int64 = 0
@@ -48,7 +52,7 @@ function run()
 	#R = 0.5 * ones(number_of_particles, number_of_properties)
 	R[:, 1] = 0.5 * ones(number_of_particles, 1)
 	R[:, 2] = 1.0 * ones(number_of_particles, 1)
-	#R[:, 3] = 1.5 * ones(number_of_particles, 1)
+	R[:, 3] = 1.5 * ones(number_of_particles, 1)
 	
 	# Positions.
 	X::Array{Float64, 1} = Lx * rand(number_of_particles)
@@ -131,7 +135,29 @@ function run()
 	# Relax system until zero energy is reached.
 	(X, Y, Z, Q0, Q1, Q2, Q3, A11, A12, A13, A21, A22, A23, A31, A32, A33, sigma_translation, sigma_rotation) = 
 		relax_system(Lx, Ly, Lz, particle_type, R, X, Y, Z, Q0, Q1, Q2, Q3, A11, A12, A13, A21, A22, A23, A31, A32, A33, sigma_translation, sigma_translation_ub, sigma_rotation, sigma_rotation_ub)
-
+	
+	println((sigma_translation, sigma_rotation))
+	
+	#################################################################################################
+#	currentA = 4
+#	currentB = 8
+#	xAB = signed_distance_mod(X[currentA], X[currentB], Lx)
+#	yAB = signed_distance_mod(Y[currentA], Y[currentB], Ly)
+#	zAB = signed_distance_mod(Z[currentA], Z[currentB], Lz)
+#	println("asdasdasfasjfkasjflkasjlkasj")
+#	println((xAB,yAB,zAB))
+#	overlapfun = overlap_ellipsoid(xAB, yAB, zAB, A11[currentA], A12[currentA], A13[currentA], A21[currentA], A22[currentA], A23[currentA], A31[currentA], A32[currentA], A33[currentA], A11[currentB], A12[currentB], A13[currentB], A21[currentB], A22[currentB], A23[currentB], A31[currentB], A32[currentB], A33[currentB], R[currentA, 1]^2 * R[currentA, 2]^2 * R[currentA, 3]^2)
+#	println(overlapfun)
+#	(a11, a12, a13, a21, a22, a23, a31, a32, a33) = characteristic_matrix_ellipsoid(Q0[currentA], Q1[currentA], Q2[currentA], Q3[currentA], R[currentA, 1], R[currentA, 2], R[currentA, 3])
+#	println((a11, a12, a13, a21, a22, a23, a31, a32, a33))
+#	(a11, a12, a13, a21, a22, a23, a31, a32, a33) = characteristic_matrix_ellipsoid(Q0[currentB], Q1[currentB], Q2[currentB], Q3[currentB], R[currentB, 1], R[currentB, 2], R[currentB, 3])
+#	println((a11, a12, a13, a21, a22, a23, a31, a32, a33))
+	
+						
+						
+	#################################################################################################
+	
+	
 	# Write result to file.
 	file_name_output::String = "output.dat"
 	file_stream_output::IOStream = open(file_name_output, "w")
