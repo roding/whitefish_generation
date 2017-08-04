5bfc5ba74b184c5f636bdef2f9767a626f7694b2
Magnus Röding <magnus.roding@gmail.com>
2017-08-04 08:51:14 +0200

    Cuboid overlap iterations changed from 20 to 10

--- src/overlap_cuboid.jl
+++ src/overlap_cuboid.jl
@@ -30,15 +30,15 @@ function overlap_cuboid(	xAB::Float64,
 	overlapfun_binary::Float64 = 0.0
 
 	overlapfun_binary = overlap_cuboid_binary(xAB, yAB, zAB, a11, a12, a13, a21, a22, a23, a31, a32, a33, b11, b12, b13, b21, b22, b23, b31, b32, b33, r1a, r2a, r3a, r1b, r2b, r3b)
-	
+
 	if overlapfun_binary == 0.0
 		return 0.0
 	else
 		gamma_min::Float64 = 0.0
 		gamma_max::Float64 = 1.0
 		gamma::Float64 = 0.5
-		number_of_iterations::Int64 = 20
-		
+		number_of_iterations::Int64 = 10
+
 		for current_iteration = 1:number_of_iterations
 			if overlap_cuboid_binary(xAB, yAB, zAB, a11, a12, a13, a21, a22, a23, a31, a32, a33, b11, b12, b13, b21, b22, b23, b31, b32, b33, gamma * r1a, gamma * r2a, gamma * r3a, gamma * r1b, gamma * r2b, gamma * r3b) == 1.0
 				gamma_max = gamma
@@ -47,7 +47,7 @@ function overlap_cuboid(	xAB::Float64,
 			end
 			gamma = 0.5 * (gamma_min + gamma_max)
 		end
-		
+
 		overlapfun = 1.0 - gamma
 	end
 end
